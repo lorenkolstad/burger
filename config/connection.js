be7bc61660aac13b4e125580ed94201457d385cd
Loren Kolstad <lorenkolstad@gmail.com>
2020-02-26 19:38:14 -0800

    Added code to the connection.js to match the jawsDB database.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 let mysql = require("mysql");
 
 let connection = mysql.createConnection({
-    host: "localhost",
+    host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    user: "a1ae8tl4ix8awu2t",
+    password: "vwpjbb67ymg8ykpt",
+    database: "yzizzfxsd30i21dd"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("Error connecting: " + err.stack);
         return;
